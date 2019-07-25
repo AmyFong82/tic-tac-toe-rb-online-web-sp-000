669aecfe621a6d14e4d5bd6ef4465e2381dc8a41
Amy Fong <amyhfong@gmail.com>
2019-07-25 17:03:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,8 +19,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(num)
-  num = num.to_i - 1
+def input_to_index(input)
+  input.to_i - 1
 end
 
 def move(board, index, player)
